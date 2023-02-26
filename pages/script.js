19a31239c017f566c8550d9993375d7d92682613
dbakinovskaya <d.bakinovskaya@gmail.com>
2023-02-26 23:30:20 +0400

    feat: add header of website

--- pages/script.js
+++ pages/script.js
@@ -6,4 +6,10 @@
             console.log(data);
         }); */
 
+const dom = {
+    main: document.querySelector('main'),
+}
 
+const header = document.createElement('header');
+header.innerHTML = '<img src="../assets/icons/logoicon.png" alt="logo of website"> <h1>Book Shop</h1>';
+dom.main.append(header);
