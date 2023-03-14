d85384dda47e02e6dfbcb7ba0bd7239496dbe6cf
dbakinovskaya <d.bakinovskaya@gmail.com>
2023-03-15 01:37:07 +0400

    feat: add validation of delivery info

--- pages/form/script.js
+++ pages/form/script.js
@@ -5,10 +5,22 @@ function buildError(elem) {
     div.className = "error__msg";
     div.innerHTML = "The field is invalid";
 
-    div.style.left = (coords.left+50) + 'px';
+    div.style.left = (coords.left+48) + 'px';
     div.style.top = (coords.top - 16 + window.pageYOffset) + 'px';
 
     return div;
+}
+
+function showErrorMsg(elem) {
+    const errorMsg = buildError(elem);
+
+    elem.classList.add('error');
+    elem.after(errorMsg);
+}
+
+function deleteErrorMsg(elem) {
+    elem.classList.remove('error');
+    elem.nextSibling.remove();
 }
 
 function checkPersonalInfo(field) {
@@ -17,13 +29,28 @@ function checkPersonalInfo(field) {
     const number = /[0-9]/.test(field.value);
     const space = /\s/.test(field.value);
 
-    if (nameReq || surnameReq || number || space) {
-        const errorMsg = buildError(field);
+    (nameReq || surnameReq || number || space) ? showErrorMsg(field) : deleteErrorMsg(field);
+}
 
-        field.classList.add('error');
-        field.after(errorMsg);
-    } else {
-        field.classList.remove('error');
-        field.nextSibling.remove();
+function getDeliveryDate() {
+    const date = new Date();
+    const day = date.getDate() +1;
+    let month = date.getMonth() +1;
+    const year = date.getFullYear();
+
+    if (month <= 9) {
+        month = '0' + month
     }
+
+    return `${year}-${month}-${day}`;
+}
+date.setAttribute('min', getDeliveryDate());
+
+function checkDeliveryInfo(field) {
+    const streetReq = ((field.id === 'street') && (field.value.length < 5));
+    const houseReq = ((field.id === 'house') && (field.value <= 0 || isNaN(field.value)));
+    const flatReq = ((field.id === 'flat') && (field.value.startsWith('-') || isNaN(field.value.replace('-','')) || (field.value.replace('-','')) <= 0));
+    const dateReq = ((field.id === 'date') && !field.value);
+
+    (streetReq || houseReq || flatReq || dateReq) ? showErrorMsg(field) : deleteErrorMsg(field);
 }
